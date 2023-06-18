a60ac4c6c4147d0689e05664a0e46651e6fcc22e
Ashley Frank <ashley.frank@ufl.edu>
2023-06-17 21:05:33 -0400

    removed comments and added test code variable

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 var form = document.getElementById('add-todo')
+
+//define variable for test code
+const todoList = document.getElementById('todo-list')
+
 form.onsubmit = function(e){
     //stop form from auto submitting
     e.preventDefault()
-    console.log('submitted')
-
-    // access the data entered by user
+    
+    // access the data etodoElntered by user
     var dataEl = document.querySelector('input').value
-    console.log(dataEl)
-
+    
     //validate user input: void if string only contains whitespace (ie. spaces, tabs, null or line breaks
     if (!dataEl.replace(/\s/g, '').length) {
       console.log('string only contains whitespace (ie. spaces, tabs or line breaks)')
       return
     }
 
-
     //access To Do List
     var ulEl = document.getElementById('todo-list')
-    //access placeholder
-    
     
     //create new list and button item
     var liElement = document.createElement('li') 
@@ -29,16 +28,12 @@ form.onsubmit = function(e){
 
     //appended button to the list item
     liElement.appendChild(buttonElement)
-    console.log(liElement)
     
     //appended list item to list
     ulEl.appendChild(liElement)
 
-    console.log(ulEl)
-
     //when button clicked, call function to add strike or remove
     buttonElement.addEventListener('click', strike)
-    console.log(buttonElement.innerHTML + "inner HTML")
 
     //add strike to completed task (or) delete already striked task to remove
     function strike(){
@@ -46,9 +41,7 @@ form.onsubmit = function(e){
             this.parentElement.remove(liElement);
         }
         else{
-           //add strike \
-        console.log('striked')
-        buttonElement.style.textDecoration = 'line-through'
+            buttonElement.style.textDecoration = 'line-through'
         }
     }
 
@@ -56,3 +49,4 @@ form.onsubmit = function(e){
     document.querySelector('input').value = ''
 
 }
+
